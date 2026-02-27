ab3c64ced581bccbce3d1118176e7f7700c8759a
Thingholm <rasmusthingholm@gmail.com>
2026-02-27 20:47:40 +0100

    Added contact mail to footer

--- client/src/components/layout/Footer.tsx
+++ client/src/components/layout/Footer.tsx
@@ -3,7 +3,8 @@ export default function Footer() {
         <footer className="p-4 md:px-16 lg:px-32 bg-gray-50 border-t border-gray-200 grid gap-4">
             <h2 className="font-semibold text-center text-gray-500">Hvad skal jeg stemme</h2>
             <p className="text-gray-500 text-center text-pretty text-xs">Hjemmesiden er designet, udviklet og vedligeholdt af Mikkel Thingholm og Rasmus Thingholm.</p>
-            <a href="https://github.com/Thingholm/hvadskaljegstemme.dk" target="_blank" className="text-gray-500 text-center text-pretty text-xs underline">Github repository</a>
+            <p className="text-gray-500 text-center text-pretty text-xs">Kontakt på <a href="mailto:rasmuskthingholm@gmail.com" className="underline">rasmuskthingholm@gmail.com</a></p>
+            <a href="https://github.com/Thingholm/hvadskaljegstemme.dk" target="_blank" className="text-gray-500 text-center text-pretty text-xs underline">GitHub repository</a>
         </footer>
     )
 }
