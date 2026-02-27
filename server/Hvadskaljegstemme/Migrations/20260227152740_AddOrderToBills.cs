372c1a88490e1f1e735726ce12abd897e17e6682
MikkelThingholm <mikkelthingholm@gmail.com>
2026-02-27 16:31:46 +0100

    Add order to bill model and sort by it in /bills endpoint

---
+++ server/Hvadskaljegstemme/Migrations/20260227152740_AddOrderToBills.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Hvadskaljegstemme.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOrderToBills : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "order",
+                table: "bills",
+                type: "integer",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "order",
+                table: "bills");
+        }
+    }
+}
